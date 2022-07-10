3e9a561e8e4986a71990642ff5b4207a45af9b92
unknown <samanyu6@yahoo.co.in>
2022-07-10 22:07:04 +0530

    added audio processor with the rust callback fn

---
+++ wasm-audio-rs/src/lib.rs
@@ -0,0 +1,52 @@
+use pitch_detection::{McLeodDetector, PitchDetector};
+
+// lib to bind code to JS, essentially calling out pitch detection libs out to JS
+use wasm_bindgen::prelude::*;
+mod utils;
+
+#[wasm_bindgen]
+pub struct PitchDetectorWasm {
+    sample_rate: usize,
+    fft_size: usize,
+    detector: McLeodDetector<f32>
+}
+
+#[wasm_bindgen]
+impl PitchDetectorWasm {
+    pub fn new(sample_rate: usize, fft_size: usize) -> PitchDetectorWasm {
+        utils::set_panic_hook();
+
+        // helps smooth the results
+        let fft_pad = fft_size / 2;
+
+        PitchDetectorWasm {
+            sample_rate,
+            fft_size,
+            detector: McLeodDetector::<f32>::new(fft_size, fft_pad),
+        }
+    }
+
+    pub fn detect_pitch(&mut self, audio_samples: Vec<f32>) -> f32 {
+        if audio_samples.len() < self.fft_size {
+            panic!("Insufficient samples passed to detect_pitch(). Expected an array containing {} elements but got {}", self.fft_size, audio_samples.len());
+        }
+
+        // min power threshold of note, for it to be processed
+        const POWER_THRESHOLD: f32 = 5.0;
+
+        // min clarity threshold of note for it to be processed/ detected
+        const CLARITY_THRESHOLD: f32 = 0.6;
+
+        let optional_pitch =  self.detector.get_pitch(
+            &audio_samples,
+            self.sample_rate,
+            POWER_THRESHOLD,
+            CLARITY_THRESHOLD,
+          );
+
+          match optional_pitch {
+            Some(pitch) => pitch.frequency,
+            None => 0.0
+          }
+    }
+}
